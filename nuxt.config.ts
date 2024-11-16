ace94bf81eedc72990172854b1337a03d5cfd8dd
Michael Synan <hello@michaelsynan.com>
2024-11-15 19:21:22 -0500

    feat: generated for appflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   modules: ["@nuxt/ui", "@nuxtjs/ionic"],
-  compatibilityDate: "2024-11-15"
-})
+  compatibilityDate: "2024-11-15",
+});
